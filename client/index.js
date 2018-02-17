1f7188e3d1c133b24354f7361a60891f5b5d67d2
tonyrud <tonrudny@gmail.com>
2018-02-17 15:18:33 -0500

    add logout mutation

--- client/index.js
+++ client/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ApolloClient from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
 
 const Root = () => {
-  return (
-    <div>
-      Auth Starter
-    </div>
-  );
-};
+  return <div>Auth Starter</div>
+}
 
-ReactDOM.render(<Root />, document.querySelector('#root'));
+ReactDOM.render(<Root />, document.querySelector('#root'))
