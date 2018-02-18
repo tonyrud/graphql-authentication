b4db904f94990960de76c28e8bd0b1eaa3740e04
tonyrud <tonrudny@gmail.com>
2018-02-18 11:26:29 -0500

    add id to user type

--- client/index.js
+++ client/index.js
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ApolloClient from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
+import { Router, hashHistory, Route, IndexRoute } from 'react-router'
+
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id,
+})
 
 const Root = () => {
-  return <div>Auth Starter</div>
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App} />
+      </Router>
+    </ApolloProvider>
+  )
 }
-
 ReactDOM.render(<Root />, document.querySelector('#root'))
