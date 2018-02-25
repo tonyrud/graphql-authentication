76eea997d97ca1fc79209fb5e840a150be8fdffc
tonyrud <tonrudny@gmail.com>
2018-02-24 22:13:39 -0500

    add login/logout buttons

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react'
 import { graphql } from 'react-apollo'
 import query from '../queries/CurrentUser'
+import { Link } from 'react-router'
 
 class Header extends Component {
+  renderButtons() {
+    const { loading, user } = this.props.data
+
+    if (loading) {
+      return <div />
+    }
+
+    if (user) {
+      return <div>Logout</div>
+    } else {
+      return (
+        <div>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </div>
+      )
+    }
+  }
+
   render() {
-    console.log(this.props.data)
-    return <div>Header</div>
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo">
+            Home
+          </Link>
+          <ul className="right">{this.renderButtons()}</ul>
+        </div>
+      </nav>
+    )
   }
 }
 
